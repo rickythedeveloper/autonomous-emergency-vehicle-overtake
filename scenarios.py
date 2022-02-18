dfff7f32ec6afe0acae873a77abaa6c5ed90ffc4
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2022-02-18 17:57:47 +0000

    #2 making scenarios easier

--- scenarios.py
+++ scenarios.py
@@ -1,26 +1,37 @@
 from utils import Position, CellType
+from typing import List, Callable
 
 # TODO take orientation into account
 def is_within_vehicle(relative_position: Position):
 	return -1 < relative_position.x < 1 and -2 < relative_position.y < 2
 
-def scenario1(position: Position) -> CellType:
-	center = Position(5, 10)
-	if position.distance_to(center) < 5:
-		return CellType.emergency
-	return CellType.road
+def make_scenario(
+	civilian_vehicle_positions: List[Position],
+	emergency_vehicle_positions: List[Position],
+	obstacle_check: Callable[[Position], bool]
+) -> Callable[[Position], CellType]:
+	def scenario(position: Position) -> CellType:
+		for p in civilian_vehicle_positions:
+			if is_within_vehicle(p - position):
+				return CellType.civilian
+		for p in emergency_vehicle_positions:
+			if is_within_vehicle(p - position):
+				return CellType.emergency
+		if obstacle_check(position):
+			return CellType.obstacle
+		return CellType.road
 
-def scenario2(position: Position) -> CellType:
-	if -1 < position.x < 0 or 10 < position.x < 11:
-		return CellType.obstacle
+	return scenario
 
-	civilian_positions = [Position(2, 15), Position(5, 10)]
-	for pos in civilian_positions:
-		if is_within_vehicle(position - pos):
-			return CellType.civilian
+scenario1 = make_scenario(
+	[Position(2, 15), Position(5, 10)],
+	[Position(8, 10)],
+	lambda position: -1 < position.x < 0 or 10 < position.x < 11
+)
 
-	emergency_position = Position(8, 10)
-	if is_within_vehicle(position - emergency_position):
-		return CellType.emergency
+scenario2 = make_scenario(
+	[Position(2, 20), Position(5, 5)],
+	[Position(5, 15)],
+	lambda position: -1 < position.x < 0 or 10 < position.x < 11
+)
 
-	return CellType.road
