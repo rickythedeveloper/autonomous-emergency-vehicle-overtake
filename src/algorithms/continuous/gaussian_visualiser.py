406f1a6fbb1efc29518e043bf26fade03fd571ec
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2022-04-17 11:01:10 +0100

    #31 do some necessary gaussian visualisation

--- src/algorithms/continuous/gaussian_visualiser.py
+++ src/algorithms/continuous/gaussian_visualiser.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 from src.models.continuous.ContinuousVehicle import VehicleType
 from src.utils.Vector2 import Vector2
-from src.algorithms.continuous.gaussian import weight_density_generator_road_and_vehicle
+from src.algorithms.continuous.gaussian import weight_density_generator_road_and_vehicle, weight_density_generator_with_road
 
 visualisation_angle = np.pi / 6
 STEP = visualisation_angle / 100
@@ -15,10 +15,10 @@ def visualise_normalised_gaussian(function: Callable[[float], float]):
 	theta = np.arange(-visualisation_angle / 2, visualisation_angle / 2 + STEP, STEP)
 	y = [function(t) for t in theta]
 
-	plt.title("Line graph")
-	plt.xlabel("theta")
-	plt.ylabel("value")
-	plt.plot(theta, y, color="green")
+	plt.title("Relative likelihood of picking a given angle")
+	plt.xlabel("angle")
+	plt.ylabel("acceptance probability")
+	plt.plot(theta * 180 / np.pi, y, color="green")
 	plt.show()
 
 @dataclass
@@ -31,12 +31,16 @@ class TestCase:
 if __name__ == '__main__':
 	# traffic_positions = [Vector2(5, 5), Vector2(0, 10), Vector2(5, 15), Vector2(-5, 5), Vector2(0, 10), Vector2(-5, 15)]
 	# traffic_positions = [Vector2(1, 1), Vector2(-1, 1), Vector2(1, 2)]
-	# traffic_positions = []
 	# traffic_positions = [Vector2(3, 10), Vector2(1, 10), Vector2(5, 15)]
 	# traffic_positions = [Vector2(3, 10), Vector2(0, 10), Vector2(5, 15), Vector2(-2, 10), Vector2(-2, 7)]
-	traffic_positions = [Vector2(5, 0)]
+	# traffic_positions = [Vector2(5, 0)]
+	# traffic_positions = [Vector2(-0.1, 5)]
+	traffic_positions = [Vector2(3, 7), Vector2(8, 2)] # this is a nice one
+	traffic_positions = [Vector2(0.1, 5), Vector2(-3, 3)]  # this represents the bad edge case
 	vehicle_types = [VehicleType.civilian for _ in range(len(traffic_positions))]
 	road_heading = 0
 	cone_angle = np.pi / 6
+
 	gaussian_function = weight_density_generator_road_and_vehicle(traffic_positions, vehicle_types, road_heading, cone_angle)
+	# gaussian_function_road = weight_density_generator_with_road(road_heading) # use this to show road gaussian on its own
 	visualise_normalised_gaussian(gaussian_function)
